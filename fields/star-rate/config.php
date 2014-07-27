45a808ee97a6454bd3eaa30379ce8e44a6f93d96
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-27 13:51:03 +0000

    1.0.91 update -fix preview
    git-svn-id: https://plugins.svn.wordpress.org/caldera-forms/tags/1.0.91@955570 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fields/star-rate/config.php
@@ -0,0 +1,61 @@
+<div class="caldera-config-group">
+	<label for="{{_id}}_number"><?php echo __('Number of Stars', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<input id="{{_id}}_number" type="number" class="field-config" name="{{_name}}[number]" value="{{number}}" style="width:70px;">
+	</div>
+</div>
+<div class="caldera-config-group">
+	<label for="{{_id}}_type"><?php echo __('Star Type', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<select id="{{_id}}_type" class="field-config" name="{{_name}}[type]">
+			<option value="star"><?php echo __('Star', 'caldera-forms'); ?></option>
+			<option value="heart"><?php echo __('Heart', 'caldera-forms'); ?></option>
+			<option value="face"><?php echo __('Face', 'caldera-forms'); ?></option>
+			<option value="dot"><?php echo __('Dot', 'caldera-forms'); ?></option>
+		</select>
+	</div>
+</div>
+<div class="caldera-config-group">
+	<label for="{{_id}}_size"><?php echo __('Star Size', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<input id="{{_id}}_size" type="number" class="field-config" name="{{_name}}[size]" value="{{size}}" style="width:70px;">px
+	</div>
+</div>
+<div class="caldera-config-group">
+	<label for="{{_id}}_space"><?php echo __('Star Spacing', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<input id="{{_id}}_space" type="number" class="field-config" name="{{_name}}[space]" value="{{space}}" style="width:70px;">px
+	</div>
+</div>
+<div class="caldera-config-group">
+	<label for="{{_id}}_single"><?php echo __('Single Select', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<input id="{{_id}}_single" type="checkbox" class="field-config" name="{{_name}}[single]" value="1" {{#if single}}checked="checked"{{/if}}>
+	</div>
+</div>
+
+<div class="caldera-config-group">
+	<label for="{{_id}}_color"><?php echo __('Star Color', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<input type="text" class="minicolor-picker field-config" id="{{_id}}_color" name="{{_name}}[color]" value="{{color}}">
+	</div>
+</div>
+<div class="caldera-config-group">
+	<label for="{{_id}}_track_color"><?php echo __('Track Color', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<input type="text" class="minicolor-picker field-config" id="{{_id}}_track_color" name="{{_name}}[track_color]" value="{{track_color}}">
+	</div>
+</div>
+<div class="caldera-config-group">
+	<label for="{{_id}}_cancel"><?php echo __('Include Cancel', 'caldera-forms-register'); ?></label>
+	<div class="caldera-config-field">
+		<input id="{{_id}}_cancel" type="checkbox" class="field-config" name="{{_name}}[cancel]" value="1" {{#if cancel}}checked="checked"{{/if}}>
+	</div>
+</div>
+
+{{#script}}
+jQuery(function($){
+	jQuery('#{{_id}}_color').miniColors();
+	jQuery('#{{_id}}_track_color').miniColors();
+});
+{{/script}}
