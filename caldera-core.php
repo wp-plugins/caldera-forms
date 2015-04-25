aa98fabff46afaa899837724d3e8e5f9faf9604a
Desertsnowman <Desertsnowman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-25 07:38:14 +0000

    wp version compatible and fix for widgets
    git-svn-id: https://plugins.svn.wordpress.org/caldera-forms/tags/1.1.9.8@1145402 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ caldera-core.php
@@ -0,0 +1,45 @@
+<?php
+/*
+  Plugin Name: Caldera Forms
+  Plugin URI: http://digilab.co.za
+  Description: Easy to use, grid based responsive form builder for creating simple to complex forms.
+  Author: David Cramer
+  Version: 1.1.9.8
+  Author URI: http://digilab.co.za
+ */
+
+//initilize plugin
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define('CFCORE_PATH', plugin_dir_path(__FILE__));
+define('CFCORE_URL', plugin_dir_url(__FILE__));
+define('CFCORE_VER', '1.1.9.8');
+define('CFCORE_EXTEND_URL', 'https://api.calderaforms.com/1.0/');
+
+include_once CFCORE_PATH . 'classes/core.php';
+include_once CFCORE_PATH . 'classes/widget.php';
+include_once CFCORE_PATH . 'classes/sanitize.php';
+
+// includes
+include_once CFCORE_PATH . 'includes/cf-ajax/plugin.php';
+include_once CFCORE_PATH . 'includes/field_processors.php';
+include_once CFCORE_PATH . 'includes/custom_field_class.php';
+include_once CFCORE_PATH . 'includes/filter_addon_plugins.php';
+
+// table builder
+register_activation_hook( __FILE__, array( 'Caldera_Forms', 'activate_caldera_forms' ) );
+
+add_action( 'plugins_loaded', array( 'Caldera_Forms', 'get_instance' ) );
+
+// Admin & Admin Ajax stuff.
+if ( is_admin() || defined( 'DOING_AJAX' ) ) {
+
+	require_once( CFCORE_PATH . 'classes/admin.php' );
+	add_action( 'plugins_loaded', array( 'Caldera_Forms_Admin', 'get_instance' ) );
+
+}
+
---
+++ includes/field_processors.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+
+add_filter('caldera_forms_process_field_file', 'cf_handle_file_upload', 10, 3);
+
+
+function cf_handle_file_upload($entry, $field, $form){
+
+	if(!empty($_FILES[$field['ID']]['size'])){
+		// check is allowed 
+		if(!empty($field['config']['allowed'])){
+			$types = explode(',',$field['config']['allowed']);
+
+			foreach($types as &$type){
+				$type=trim($type);
+			}
+			$check = pathinfo($_FILES[$field['ID']]['name']);
+			if(!in_array( $check['extension'], $types)){
+				if(count($types) > 1){
+					return new WP_Error( 'fail', __('File type not allowed. Allowed types are: ', 'caldera-forms') . ' '. implode(', ', $types) );
+				}else{
+					return new WP_Error( 'fail', __('File type needs to be', 'caldera-forms') . ' .' . $types[0] );					
+				}
+			}
+
+		}
+		if ( ! function_exists( 'wp_handle_upload' ) ) require_once( ABSPATH . 'wp-admin/includes/file.php' );
+
+		$upload = wp_handle_upload($_FILES[$field['ID']], array( 'test_form' => false ), date('Y/m') );
+
+		if(empty($upload['error'])){
+			return $upload['url'];
+		}else{
+			return new WP_Error( 'fail', $upload['error'] );
+		}
+	}
+}
